246316c5b8181f5beeedeb8841ccd351901a7ab9
Sun1203 <84012715+Sun1203@users.noreply.github.com>
2022-10-14 18:56:53 +0900

    상속

---
+++ src/inheritance/sec01/exam04/SupersonicAirplaneExample.java
@@ -0,0 +1,13 @@
+package inheritance.sec01.exam04;
+
+public class SupersonicAirplaneExample {
+
+    public static void main(String[] args) {
+        SupersonicAirplane sa = new SupersonicAirplane();
+        sa.takeOff();
+        sa.fly();
+        sa.flyMode = SupersonicAirplane.SUPERSONIC;
+        sa.fly();
+        sa.land();
+    }
+}
